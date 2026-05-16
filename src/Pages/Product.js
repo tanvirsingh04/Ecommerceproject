a572e33cbcf29b4370568645ff5d3a74d2a5b4bb
Tanvir Singh <chandel.tanvir55@gmail.com>
2026-05-16 11:58:36 +0530

    Created login and Registeration function and implemented and is working.

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,12 +1,26 @@
 import React from "react";
 // import './Product.css'
 import Navbar from "./navbar";
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
 import './navbar.css';
-function Product(){
-    return(
-        <div>
-            <Navbar/>
-        </div>
-    )
+function Product() {
+const { theme, toggleTheme } = useContext(ThemeContext);
+const styles = {
+backgroundColor: theme === "light" ? "#fff" : "#333" ,
+color: theme === "light" ? "#000" : "#fff" ,
+height: "100vh" ,
+textAlign: "center" ,
+paddingTop: "50px"
 };
+return (
+<div style={styles}>
+<h1>{theme.toUpperCase()} MODE</h1>
+<button onClick={toggleTheme}>
+Switch Theme
+</button>
+</div>
+);
+}
+// export default App;
 export default Product;
